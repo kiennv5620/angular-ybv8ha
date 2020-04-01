6d6f1db2516e0f241bdee26d227f65b0950cba19
lien5620 <kiez5620@gmail.com>
2020-04-01 15:17:45 +0700

    5 files modified, 9 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { ProductListComponent } from './admin/product-list/product-list.component';
+import { ProductAddComponent } from './admin/product-add/product-add.component';
+import { ProductDetailComponent } from './admin/product-detail/product-detail.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: AdminComponent}
+  {path: 'home', component: ProductListComponent},
+  {path: 'product-list', component: ProductListComponent},
+  {path: 'product/add', component: ProductAddComponent},
+  {path: 'product/:productID', component: ProductDetailComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AdminComponent } from './admin/admin.component';
 import { ProductService } from './product.service';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './admin/product-list/product-list.component';
+import { ProductDetailComponent } from './admin/product-detail/product-detail.component';
+import { ProductAddComponent } from './admin/product-add/product-add.component';
 
 @NgModule({
   imports:      [
@@ -15,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     HttpClientModule],
-  declarations: [ AppComponent, HelloComponent, AdminComponent ],
+  declarations: [ AppComponent, HelloComponent, AdminComponent, ProductListComponent, ProductDetailComponent, ProductAddComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
